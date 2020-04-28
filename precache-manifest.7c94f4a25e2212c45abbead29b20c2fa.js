0ca7652c9f7e052498ecece983b848669cb2b121
Cristian Cordoba <cristian.cordoba@blackboard.com>
2020-04-28 02:30:26 -0500

    Updates

---
+++ precache-manifest.7c94f4a25e2212c45abbead29b20c2fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f4ed7da29f9d9c57ac7ce8711d399b",
+    "url": "/tower-f/index.html"
+  },
+  {
+    "revision": "11665becb13f4eac1ad7",
+    "url": "/tower-f/static/css/main.c48c4137.chunk.css"
+  },
+  {
+    "revision": "a09f5ebfeddd459b879e",
+    "url": "/tower-f/static/js/2.c0ed4786.chunk.js"
+  },
+  {
+    "revision": "11665becb13f4eac1ad7",
+    "url": "/tower-f/static/js/main.ec1f0b64.chunk.js"
+  },
+  {
+    "revision": "697843aba2f76544d22c",
+    "url": "/tower-f/static/js/runtime~main.10e61bf2.js"
+  }
+]);
